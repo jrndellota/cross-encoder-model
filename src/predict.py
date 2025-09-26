e757b1d6c1f6cb01a57b285d70895e6448ed7c77
jrndellota <jaron@thinkingmachin.es>
2025-09-27 00:15:47 +0800

    Add other relevant fields

--- src/predict.py
+++ src/predict.py
@@ -1,9 +1,10 @@
 from sentence_transformers import CrossEncoder
 import pandas as pd
 import json
+from tqdm import tqdm
 
 restaurants_df = pd.read_parquet("data/restaurants.parquet").set_index("restaurant_id")
-queries_dev_df = pd.read_csv("data/queries_dev.csv")
+queries_dev_df = pd.read_csv("data/queries_dev.csv").set_index("query_id")
 candidates_dev_df = pd.read_json("data/bm25_candidates_dev_top500.jsonl", lines=True)
 
 model = CrossEncoder("models/cross-encoder")
@@ -12,9 +13,7 @@ output_path = "runs/test_predictions.jsonl"
 
 print(f"Number of candidate rows: {len(candidates_dev_df)}")
 
-with open(output_path, "w+", encoding="utf-8") as f:
-    from tqdm import tqdm
-
+with open(output_path, "w", encoding="utf-8") as f:
     for i, row in tqdm(
         candidates_dev_df.iterrows(),
         total=len(candidates_dev_df),
@@ -22,16 +21,23 @@ with open(output_path, "w+", encoding="utf-8") as f:
     ):
         query_id = row["query_id"]
         candidates = row["candidates"]
-        query = queries_dev_df.loc[
-            queries_dev_df["query_id"] == query_id, "query_text"
-        ].values[0]
-
+        query = queries_dev_df.loc[query_id, "query_text"]
         pairs = []
         restaurant_ids = []
         for candidate in candidates:
-            restaurant_ids.append(candidate["restaurant_id"])
-            description = restaurants_df.loc[candidate["restaurant_id"], "description"]
-            pairs.append([query, description])
+            restaurant_id = candidate["restaurant_id"]
+            restaurant_ids.append(restaurant_id)
+            r_text = (
+                "Neighborhood: "
+                + restaurants_df.loc[restaurant_id, "neighborhood"]
+                + " | Cuisines: "
+                + restaurants_df.loc[restaurant_id, "cuisines"]
+                + " | Description: "
+                + restaurants_df.loc[restaurant_id, "description"]
+                + " | Tags: "
+                + restaurants_df.loc[restaurant_id, "tags"]
+            )
+            pairs.append([query, r_text])
 
         scores = model.predict(pairs, convert_to_numpy=True)
         ranked = sorted(
--- src/train.py
+++ src/train.py
@@ -3,43 +3,49 @@ from torch.utils.data import DataLoader
 from sentence_transformers import CrossEncoder, InputExample
 
 restaurants_df = pd.read_parquet("data/restaurants.parquet").set_index("restaurant_id")
-
-
-queries_train_df = pd.read_csv("data/queries_train.csv")
+queries_train_df = pd.read_csv("data/queries_train.csv").set_index("query_id")
 qrels_train_df = pd.read_csv(
     "data/qrels_train.tsv", sep="\t", names=["query_id", "restaurant_id", "relevance"]
 )
-qrels_dev_df = pd.read_csv(
-    "data/qrels_dev.tsv", sep="\t", names=["query_id", "restaurant_id", "relevance"]
-)
+
 model_name = "cross-encoder/ms-marco-MiniLM-L12-v2"
 
 
-model = CrossEncoder(model_name, num_labels=1, max_length=512, device="cuda")
+model = CrossEncoder(model_name, num_labels=1, max_length=384, device="cuda")
 
 train_examples = []
 for _, row in qrels_train_df.iterrows():
-    query = queries_train_df.loc[
-        queries_train_df["query_id"] == row["query_id"], "query_text"
-    ].values[0]
-    restaurant = restaurants_df.loc[row["restaurant_id"], "description"]
-    train_examples.append(
-        InputExample(texts=[query, restaurant], label=row["relevance"])
+    query_id = row["query_id"]
+    query = queries_train_df.loc[query_id, "query_text"]
+
+    # Concatenate neighborhood, cuisines, description, and tags
+    restaurant_id = row["restaurant_id"]
+    r_text = (
+        "Neighborhood: "
+        + restaurants_df.loc[restaurant_id, "neighborhood"]
+        + " | Cuisines: "
+        + restaurants_df.loc[restaurant_id, "cuisines"]
+        + " | Description: "
+        + restaurants_df.loc[restaurant_id, "description"]
+        + " | Tags: "
+        + restaurants_df.loc[restaurant_id, "tags"]
     )
+    label = row["relevance"]
+    train_examples.append(InputExample(texts=[query, r_text], label=label))
 
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
 
 model.fit(
     train_dataloader,
-    epochs=15,  # Number of training epochs
-    optimizer_params={"lr": 2e-5},
-    evaluation_steps=1000,  # Evaluate model every 1000 steps
-    warmup_steps=1000,  # Gradually increase learning rate over first 1000 steps
+    epochs=8,  # Number of training epochs
+    # optimizer_params={"lr": 2e-5},
+    evaluation_steps=2000,  # Evaluate model every 1000 steps
+    warmup_steps=100,  # Gradually increase learning rate over first 100 steps
     show_progress_bar=True,  # Display training progress
     output_path="models/cross-encoder",  # Directory to save model checkpoints
     use_amp=True,  # Use automatic mixed precision training
 )
 
-# Explicitly save the model
+# Save the model
 model.save("models/cross-encoder")
 print("Model saved successfully!")
